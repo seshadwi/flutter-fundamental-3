7de0bdadc978709d3a7748ef6bc41f06b3075389
seshadwi <seshaadwi@gamil.com>
2022-08-31 14:29:50 +0700

    praktikum 1

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: GestureDetector(
-          onTap: _incrementCounter,
+          onLongPress: _incrementCounter,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
